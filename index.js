6b951e9aa6dde5f696d8625e8928ec29bcc85ed3
Mohammad Anwari <mdamt@mdamt.net>
2014-07-10 21:40:20 +0700

    Update with new changes in bkn.go.id site

--- index.js
+++ index.js
@@ -1,56 +1,28 @@
 var request = require("request");
-var Parser = require("parse5").Parser;
-var parser = new Parser();
+var cheerio = require("cheerio");
 
-var URL = "http://www.bkn.go.id/in/pastikan-data-anda-benar.html";
+var URL = "http://www.bkn.go.id/profil-pns";
 
 var bknScrapper = function() {
 }
 
 bknScrapper.prototype.getData = function(id, cb) {
   var form = {
-    id: id
+    nip: id
   };
 
-  var traverse = function(find, node, cb) {
-    if (!node.childNodes || node.childNodes.length == 0) {
-      return false;
-    }
-    if (node.nodeName == find) {
-      cb(node);
-    }
-
-    for (var i = 0; i < node.childNodes.length; i ++) {
-      traverse(find, node.childNodes[i], cb);
-    }
-    return false;
-  }
-
   request.post(URL, { form : form}, function(err, resp, body) {
-    var doc = parser.parse(body);
-    var level = 0;
-    var node = doc;
     var data = {};
-    traverse("tbody", node, function(tbody) {
-      traverse("tr", tbody, function(tr) {
-        if (tr.childNodes) {
-          var key, value;
-          tr.childNodes.forEach(function(td) {
-            if (td.childNodes && td.childNodes.length > 0) {
-              if (td.childNodes[0].nodeName == "b") {
-                var b = td.childNodes[0];
-                key = b.childNodes[0].value;
-              } else if (td.childNodes[0].nodeName == "#text") {
-                value = td.childNodes[0].value;
-              }
-            }
-          });
-          if (key) {
-            data[key] = value;
-          }
-        }
-      })
+    var $ = cheerio.load(body);
+    var entry = $("div.pns-row");
+    entry.each(function(i, e) {
+      var key = $(e).find(".label").text(); 
+      var value = $(e).find(".value").text(); 
+      value = value.replace(": ", "");
+      value = value.replace(/ {2,}/g, "");
+      data[key] = value;
     });
+
     cb(data);
   }) 
 }
